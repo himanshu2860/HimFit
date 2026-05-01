23c9d31651a818cac98a724a475ccf2213eff5d5
Himanshu Sharma <hs6665342@gmail.com>
2026-05-01 20:15:48 +0530

    Initial commit - HimFit project

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://himfit-backend.onrender.com",
+  baseURL: "http://localhost:5000",
 });
 
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -43,7 +43,7 @@ const [proteinGoal, setProteinGoal] = useState("")
   setLoading(true);
 
   try {
-   const res = await fetch("https://himfit-backend.onrender.com/api/auth/register", {
+   const res = await fetch("http://localhost:5000/api/auth/register", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
