5f3ff729a08f021aaba2b6b0707bbc093e6e4086
Himanshu Sharma <hs6665342@gmail.com>
2026-05-01 15:46:58 +0530

    fixed now

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,32 +1,29 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
+const allowedOrigins = [
+  "https://him-fit.vercel.app",  
+  "http://localhost:5173"        
+];
 
-app.use(cors({
-  origin: [
-    "https://him-fit.vercel.app",
-    "http://localhost:5173"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors())
 
-
-const authRoutes = require("./routes/auth.routes");
-const foodRoutes = require("./routes/food.routes");
-const userRoutes = require("./routes/user.routes");
-const dietRoutes = require("./routes/diet.routes");
+const authRoutes = require("./routes/auth.routes")
+const foodRoutes = require("./routes/food.routes")
+const userRoutes = require("./routes/user.routes")
+const dietRoutes = require("./routes/diet.routes")
 const workoutRoutes = require("./routes/workout.routes");
 const aiRoutes = require("./routes/ai.routes");
 
 app.use("/api/ai", aiRoutes);
 app.use("/api/workout", workoutRoutes);
-app.use("/api/diet", dietRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/food", foodRoutes);
+app.use("/api/diet", dietRoutes)
+app.use("/api/user", userRoutes)
+app.use("/api/auth", authRoutes)
 
-module.exports = app;
+app.use("/api/food", foodRoutes)
+
+module.exports = app  
